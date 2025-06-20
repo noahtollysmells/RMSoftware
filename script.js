1efa7d473e19e7d5ca66e8da711349eb38bba639
NoahTolly <noah.tolly@icloud.com>
2025-06-20 22:31:49 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+// Your full JavaScript logic should be here
+function staffLogin() {
+  alert('Login button clicked. (Implement logic in script.js)');
+}
+function createAdmin() {
+  alert('Create admin button clicked. (Implement logic in script.js)');
+}
